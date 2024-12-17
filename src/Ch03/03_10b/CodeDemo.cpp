22c8ba8fe587a660690bfa4948556e372da56785
Chris-Winnard <74295248+Chris-Winnard@users.noreply.github.com>
2024-12-17 21:52:42 +0000

    More work

--- src/Ch03/03_10b/CodeDemo.cpp
+++ src/Ch03/03_10b/CodeDemo.cpp
@@ -8,7 +8,18 @@
 #include "cow.h"
 
 int main(){
-    
+    std::vector<cow> cattle;
+
+    cattle.push_back(cow("Betty", 6, cow_purpose::meat));
+    cattle.push_back(cow("Libby", 4, cow_purpose::hide));
+    cattle.push_back(cow("Trudy", 5, cow_purpose::pet));
+    cattle.push_back(cow("Betsy", 2, cow_purpose::dairy));
+
+    std::cout << "The first cow is named " << cattle.begin()->get_name() << std::endl;
+    std::cout << "The index 1 cow is named " << cattle[1].get_name() << std::endl;
+    std::cout << "The index 2 cow is named " << prev(cattle.end(),2)->get_name() << std::endl;
+    std::cout << "The final cow is named " << (cattle.end() -1)->get_name() << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
